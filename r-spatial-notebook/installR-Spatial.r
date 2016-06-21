e01b58b78ea2662e8b3b5510c133a9c93501bc8f
epifanio <epiesasha@gmail.com>
2016-06-21 16:04:24 +0200

    add r-spatial-notebook which includes rstudio-server and Spatial packages

---
+++ r-spatial-notebook/installR-Spatial.r
@@ -0,0 +1,6 @@
+install.packages('ctv',
+                 repos = 'http://cran.us.r-project.org')
+library('ctv')
+install.view('Spatial')
+
+q()
---
+++ r-spatial-notebook/install_rstudio-server.sh
@@ -0,0 +1,4 @@
+apt-get install gdebi-core
+wget https://download2.rstudio.org/rstudio-server-0.99.902-amd64.deb
+gdebi rstudio-server-0.99.902-amd64.deb
+rm -rf rstudio-server-0.99.902-amd64.deb
