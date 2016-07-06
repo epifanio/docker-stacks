0dbae1278ebe17ef707f85b1287d5fc29fb82f98
epifanio <epiesasha@gmail.com>
2016-07-07 01:15:37 +0200

    adding R packages for the multivariate spatial statistic class

--- r-spatial-notebook/installR-Spatial.r
+++ r-spatial-notebook/installR-Spatial.r
@@ -3,4 +3,8 @@ install.packages('ctv',
 library('ctv')
 install.views('Spatial', repos='http://cran.us.r-project.org')
 
+missingpackages <- c('adegraphics', 'adespatial', 'ape', 'cclust', 'FD', 'labdsv', 'rgl')
+for (i in missingpackages) {
+install.packages(i, repos='http://cran.us.r-project.org')
+}
 q()
