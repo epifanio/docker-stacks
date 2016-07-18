10d12de86cdaf13658448ec7f0a402343cd227f1
epifanio <epiesasha@gmail.com>
2016-07-18 22:50:43 +0200

    fix script permisions

--- extended-notebook/install_extensions.sh
+++ extended-notebook/install_extensions.sh
@@ -5,6 +5,6 @@ cd toc
 wget https://raw.githubusercontent.com/minrk/ipython_extensions/master/nbextensions/toc.js
 wget https://raw.githubusercontent.com/minrk/ipython_extensions/master/nbextensions/toc.css
 cd ..
-jupyter-nbextension install --sys toc
+jupyter-nbextension install toc
 jupyter-nbextension enable toc/toc
 rm -rf toc
