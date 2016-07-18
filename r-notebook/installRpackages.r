7ff1aa3dfafaa8ae3bd05857089bf8d905361036
epifanio <epiesasha@gmail.com>
2016-07-18 16:35:30 +0200

    attmpt to fix rkernbel installation

--- r-notebook/installRpackages.r
+++ r-notebook/installRpackages.r
@@ -1,6 +1,10 @@
-install.packages(c('repr', 'IRkernel', 'IRdisplay'),
-                 repos = c('http://irkernel.github.io/', 'http://cran.us.r-project.org'))
-IRkernel::installspec()
+#install.packages(c('repr', 'IRkernel', 'IRdisplay'),
+#                 repos = c('http://irkernel.github.io/', 'http://cran.us.r-project.org'))
+#IRkernel::installspec()
+library("devtools")
 
+install.packages(c('pbdZMQ', 'repr', 'devtools'))  # repr is already on CRAN
+# devtools::install_github('IRkernel/repr')        # or get the latest repr from master
+devtools::install_github('IRkernel/IRkernel')
 
 q()
