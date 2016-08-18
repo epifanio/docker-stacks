6d649a0bae995523534c8ae238bb04414ae35597
epifanio <epiesasha@me.com>
2016-08-18 11:18:30 -0400

    added new packages and merged installation of grass and rgl in a single Rscript

--- r-spatial-notebook/install_extrapackages.r
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/Rscript
-devtools::install_github("wesm/feather/R")
-devtools::install_github("bwlewis/rthreejs")
-install.packages("rglwidget", repos = 'http://cran.us.r-project.org')
-q()
