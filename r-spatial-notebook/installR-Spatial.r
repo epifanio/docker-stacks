5cd45b714bab70b459f9178ddb9a6e78d4b44b07
epifanio <epiesasha@me.com>
2016-08-18 11:14:12 -0400

    added new packages and merged installation of grass and rgl in a single Rscript

--- r-spatial-notebook/installR-Spatial.r
+++ r-spatial-notebook/installR-Spatial.r
@@ -3,8 +3,16 @@ install.packages('ctv',
 library('ctv')
 install.views('Spatial', repos='http://cran.us.r-project.org')
 
-missingpackages <- c('adegraphics', 'adespatial', 'ape', 'cclust', 'FD', 'labdsv', 'rgl')
+missingpackages <- c('adegraphics', 'adespatial', 'ape', 'cclust', 'FD', 'labdsv')
 for (i in missingpackages) {
 install.packages(i, repos='http://cran.us.r-project.org')
 }
+
+devtools::install_github("wesm/feather/R")
+devtools::install_github("bwlewis/rthreejs")
+install.packages("rglwidget", repos = 'http://cran.us.r-project.org')
+
+install.packages('rgrass7',
+                 repos = 'http://cran.us.r-project.org')
+
 q()
