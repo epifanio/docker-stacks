60a4609b3e8552f8d53428951a55aa191ce2ceec
epifanio <epiesasha@me.com>
2016-08-30 21:14:39 -0400

    remove debs file

--- r-notebook/install_rstudio-server.sh
+++ r-notebook/install_rstudio-server.sh
@@ -5,3 +5,4 @@ dpkg -i libssl1.0.0_1.0.1t-1+deb8u2_amd64.deb
 wget https://download2.rstudio.org/rstudio-server-0.99.903-amd64.deb
 gdebi -n rstudio-server-0.99.903-amd64.deb
 rm -rf rstudio-server-0.99.903-amd64.deb
+rm -rf libssl1.0.0_1.0.1t-1+deb8u2_amd64.deb
