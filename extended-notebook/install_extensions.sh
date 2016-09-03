72a236579ca010ca7c20025951194ce3068d9dfa
epifanio <epiesasha@me.com>
2016-09-03 07:17:33 -0400

    try to enable contrib extensions

--- extended-notebook/install_extensions.sh
+++ extended-notebook/install_extensions.sh
@@ -10,3 +10,19 @@ jupyter-nbextension enable toc/toc
 rm -rf toc
 jupyter nbextension install https://rawgithub.com/minrk/ipython_extensions/master/nbextensions/gist.js
 jupyter nbextension enable gist
+
+git clone https://github.com/Jupyter-contrib/jupyter_contrib_core
+pip3 install -e jupyter_contrib_core
+
+git clone https://github.com/ipython-contrib/IPython-notebook-extensions
+pip3 install -e jupyter_contrib_nbextensions
+
+jupyter contrib nbextension install
+
+pip2 install jupyter_nbextensions_configurator
+pip3 install jupyter_nbextensions_configurator
+
+jupyter nbextensions_configurator enable --system
+
+rm -rf IPython-notebook-extensions
+rm -rf jupyter_contrib_core
